cc0d435df31168146e7e81e861b47a5a4151b4dd
Prof. Ajit Vishwakarma <ajit@vinayavish.com>
2022-07-29 15:42:23 +0530

    Test case 1, 2 , 3 added for LOGIN Page

---
+++ Test1.java
@@ -0,0 +1,26 @@
+package test1;
+// Version #1
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		//Add Chrome Driver
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\garim\\chromedriver.exe");
+		
+		//Initialize the Chrome Driver
+		WebDriver driver =  new ChromeDriver();
+		
+		driver.navigate().to("https://www.google.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("Ajit Vishwakarma");
+		
+		driver.findElement(By.name("btnK")).submit();
+		
+
+	}
+
+}
